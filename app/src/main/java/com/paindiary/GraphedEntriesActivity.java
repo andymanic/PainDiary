b7e39b77ac0a5a44dc80a3cb55b95572bc74a69e
andymanic <andymanic@hotmail.co.uk>
2018-01-16 18:21:19 +0000

    Added create graph button and activity

--- app/src/main/java/com/paindiary/GraphedEntriesActivity.java
+++ app/src/main/java/com/paindiary/GraphedEntriesActivity.java
@@ -3,11 +3,13 @@ package com.paindiary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GraphedEntries extends AppCompatActivity {
+public class GraphedEntriesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphed_entries);
+
+
     }
 }
